5252cdac9d05253b20d69c6e10e880eb1f5e35b0
AugustDelin <august.del@live.se>
2023-10-12 10:51:42 +0200

    Update Program.cs

--- PoddProjekt/Program.cs
+++ PoddProjekt/Program.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace PoddProjekt
 {
-    internal static class Program
+    internal abstract class Program
     {
         /// <summary>
         /// The main entry point for the application.
